b4561214844e7ed377ac6baf8ffd6702a065b22b
raymondborel <rborel852@gmail.com>
2023-06-28 00:39:45 -0700

    Created Template for Home/About Routes

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
+from django.views.generic.base import TemplateView
 # Create your views here.
 
-class Home(View):
-    def get(self, request):
-        return HttpResponse("Art Collection App")
+class Home(TemplateView):
+    template_name = "home.html"
+
+class About(TemplateView):
+    template_name = "about.html"
