8acf752712c97af7a541f8df1e1b23cfe248a757
raymondborel <rborel852@gmail.com>
2023-06-27 19:28:00 -0700

    Created DB

--- art_collector/settings.py
+++ art_collector/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main_app',
 ]
 
 MIDDLEWARE = [
@@ -75,10 +76,12 @@ WSGI_APPLICATION = 'art_collector.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'art_pieces',
     }
 }
+
+
 
 
 # Password validation
