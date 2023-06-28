f0f78699517f13c426db5e2c4600eef89eebff7f
raymondborel <rborel852@gmail.com>
2023-06-28 15:02:19 -0700

    Added Artist Route with Seed Data

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.Home.as_view(), name="home"),
-    path('about/', views.About.as_view(), name="about")
+    path('about/', views.About.as_view(), name="about"),
+    path('artists/', views.ArtistList.as_view(), name="artist_list")
     ]
--- main_app/views.py
+++ main_app/views.py
@@ -9,3 +9,28 @@ class Home(TemplateView):
 
 class About(TemplateView):
     template_name = "about.html"
+
+class ArtistList(TemplateView):
+    template_name = "artist_list"
+
+class Artist:
+    def __init__(self, name, image, bio):
+        self.name = name
+        self.image = image
+        self.bio = bio
+
+
+artists = [
+  Artist("Salvador Dali", "https://mgm-website-production.oss-cn-hongkong.aliyuncs.com/uploads/2018/01/salvador-dali-1024.jpg",
+          "Salvador Domingo Felipe Jacinto Dalí i Domènech, Marquess of Dalí of Púbol, known as Salvador Dalíwas a Spanish surrealist artist renowned for his technical skill, precise draftsmanship, and the striking and bizarre images in his work."),
+  Artist("Takashi Murakami",
+          "https://malvernschool.com/wp-content/uploads/2019/05/Artist-of-the-Month.jpg", "Takashi Murakami (村上 隆, Murakami Takashi, born February 1, 1962) is a Japanese contemporary artist."),
+  Artist("Joji", "https://i.scdn.co/image/7bc3bb57c6977b18d8afe7d02adaeed4c31810df",
+          "Joji is one of the most enthralling artists of the digital age. New album Nectar arrives as an eagerly anticipated follow-up to Joji's RIAA Gold-certified first full-length album BALLADS 1, which topped the Billboard R&B / Hip-Hop Charts and has amassed 3.6B+ streams to date."),
+  Artist("Metallica",
+          "https://i.scdn.co/image/ab67706c0000da84eb6bb372a485d26fd32d1922", "Metallica formed in 1981 by drummer Lars Ulrich and guitarist and vocalist James Hetfield and has become one of the most influential and commercially successful rock bands in history, having sold 110 million albums worldwide while playing to millions of fans on literally all seven continents."),
+  Artist("Bad Bunny",
+          "https://www.clashmusic.com/sites/default/files/sty…e/Bad-Bunny-YHLQMDLG-Album-2020.jpg?itok=tbZNj82L", "Benito Antonio Martínez Ocasio, known by his stage name Bad Bunny, is a Puerto Rican rapper, singer, and songwriter. His music is often defined as Latin trap and reggaeton, but he has incorporated various other genres into his music, including rock, bachata, and soul"),
+  Artist("Kaskade",
+          "https://i1.sndcdn.com/artworks-sNjd3toBZYCG-0-t500x500.jpg", "Ryan Gary Raddon, better known by his stage name Kaskade, is an American DJ, record producer, and remixer."),
+]
