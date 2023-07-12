811d63facead520f7221c66947a84a1528640145
raymondborel <rborel852@gmail.com>
2023-07-11 23:56:26 -0700

    fixed create route

--- main_app/models.py
+++ main_app/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Artist(models.Model):
 
     name = models.CharField(max_length=100)
-    img = models.CharField(max_length=250)
+    image = models.CharField(max_length=250)
     bio = models.TextField(max_length=500)
     created_at = models.DateTimeField(auto_now_add=True)
     
--- main_app/views.py
+++ main_app/views.py
@@ -30,14 +30,13 @@ class ArtistList(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # to get the query parameter we have to acccess it in the request.GET dictionary object        
         name = self.request.GET.get("name")
-        # If a query exists we will filter by name 
         if name != None:
-            # .filter is the sql WHERE statement and name__icontains is doing a search for any name that contains the query param
             context["artists"] = Artist.objects.filter(name__icontains=name)
+            context["header"] = f"Searching for {name}"
         else:
             context["artists"] = Artist.objects.all()
+            context["header"] = "Trending Artists"
         return context
     
 
